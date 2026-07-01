7363af06fb611abc18dda7c31da53a88939dc2a8
Francis Boisvert <333powametal@gmail.com>
2026-07-01 14:51:22 -0400

    [Feat] Route pour ajouter monstres terminée

--- backend/src/routes/monstres.routes.ts
+++ backend/src/routes/monstres.routes.ts
@@ -3,7 +3,7 @@ import { monstres } from "../../api/monstres.js"
 import axios from "axios"
 import prisma from "../utils/prisma.js"
 
-const routerMonstres = Router ()
+const routeurMonstres = Router ()
 
 async function recupererMonstre(nom: string){
     try {
@@ -23,3 +23,18 @@ async function recupererMonstre(nom: string){
         return null;
     }
 }
+
+routeurMonstres.post("/ajouterMonstre/:nom", async (req: Request, res: Response)=>{
+    const donnees = await recupererMonstre(req.params.nom as string)
+    if (!donnees) {
+        return res.status(404).json({erreur: "Ce monstre n'existe pas dans le manuel des monstres"})
+    }
+    try{
+        const monstre = await prisma.Monstre.create({
+            data: donnees as any,
+        })
+        res.status(201).json({ message: `${monstre.nom} a été ajouté a dans la table: monstres de la base de données`})
+    } catch (e) {
+        res.status(400).json({erreur: "Ce monstre est deja dans la base de données"})
+    }
+})
