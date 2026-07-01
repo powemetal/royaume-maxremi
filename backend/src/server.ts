52d1bd9955ea12c133d8836b2bb10a30a33b15e4
Clement Laflamme <clement_laflamme@videotron.ca>
2026-07-01 15:13:35 -0400

    Ajout : CRUD pour les utilisateurs

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import express, { type Request, type Response } from "express"
 import dotenv from "dotenv"
+import routerUtilisateur from "./routes/utilisateur.routes.js"
 //import authRoutes from "./routes/auth.routes.js"
 dotenv.config()
 
@@ -8,12 +9,13 @@ dotenv.config()
 const app = express()
 app.use(express.json())
 
+
 app.get("/", (req: Request, res: Response) => {
     res.json({message: "API du Royaume de Maxremi!"})
 })
 
 //app.use("/auth", authRoutes)
-
+app.use("/utilisateur", routerUtilisateur)
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`Serveur sur http://localhost:${PORT}`))
