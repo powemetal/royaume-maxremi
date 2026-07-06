44de36cb7bbb7b2c7632f44ae0177d273e548c64
Pascal-the-dog <Mercierp@outlook.com>
2026-07-06 17:20:54 -0400

    feat: Ajout de inventaire.routes.ts

--- backend/src/routes/inventaire.routes.ts
+++ backend/src/routes/inventaire.routes.ts
@@ -9,21 +9,17 @@ routerInventaire.get('/inventaire/:personnageId', async (req: Request, res: Resp
   const personnageId = req.params;
 
   try {
-    const inventaire = await prisma.inventaire.findUnique({
-      where: { personnageId: personnageId },
-      include: {
-        objets: true // Inclut les détails de chaque objets dans l'inventaire
+     const inventaire = await prisma.inventaire.findMany({
+            where: { idPersonnage: personnageId }, 
+            include: {
+                objet: true // Inclut les détails de chaque objets dans l'inventaire
       }
     });
 
-    if (!inventaire) {
-      return res.status(404).json({ message: "Inventaire non trouvé pour ce personnage." });
+    return res.json(inventaire)
+    } catch (e) {
+        return res.status(500).json({ erreur: `Erreur serveur lors de la récupération de l'inventaire : ${e}` })
     }
-
-    res.json(inventaire);
-  } catch (error) {
-    res.status(500).json({ error: "Erreur de serveur lors de la récupération de l'inventaire : ${e}" });
-  }
 });
 
 //Ajouter un objet a l'inventaire
@@ -36,8 +32,8 @@ routerInventaire.post("/inventaire/ajouter", async (req: Request, res: Response)
 
      try {
         // Trouve l'objet par son nom unique dans la table global des objets
-        const objet = await prisma.objet.findFirst({
-            where: { nom: { equals: nomObjet, mode: "insensitive" } }
+        const objet = await prisma.objet.findUnique({
+            where: { nom: nomObjet }
         })
 
         if (!objet) {
@@ -45,22 +41,22 @@ routerInventaire.post("/inventaire/ajouter", async (req: Request, res: Response)
         }
 
         // Connecte directement l'objet à l'inventaire via la relation 1-1 du personnage
-        const inventaireMisAJour = await prisma.inventaire.update({
-            where: { personnageId: personnageId },
+       const nouvelItemInventaire = await prisma.inventaire.create({
             data: {
-                objets: {
-                    connect: { id: objet.id } // Connecte l'id numérique de l'objet trouvé
-                }
+                idPersonnage: personnageId,
+                idObjet: objet.id
             },
-            include: { objets: true }
+            include: {
+                objet: true
+            }
         })
 
-        return res.status(200).json({ 
-            message: `${objet.nom} a été ajouté avec succès.`,
-            inventaire: inventaireMisAJour 
+        return res.status(201).json({ 
+            message: `L'objet '${objet.nom}' a été ajouté à l'inventaire du personnage.`,
+            item: nouvelItemInventaire 
         })
     } catch (e) {
-        return res.status(500).json({ erreur: `Erreur lors de l'ajout de l'objet : ${e}. Vérifiez que l'ID du personnage est correct.` })
+        return res.status(500).json({ erreur: `Erreur lors de l'ajout de l'objet : ${e}.` })
     }
 })
 
@@ -73,32 +69,37 @@ const { personnageId, nomObjet } = req.body
     }
 
     try {
-        // Trouve l'objet par son nom unique
-        const objet = await prisma.objet.findFirst({
-            where: { nom: { equals: nomObjet, mode: "insensitive" } }
+        // Trouver l'objet global pour avoir son ID
+        const objet = await prisma.objet.findUnique({
+            where: { nom: nomObjet }
         })
 
         if (!objet) {
-            return res.status(404).json({ erreur: `L'objet ${nomObjet} n'existe pas.` })
+            return res.status(404).json({ erreur: `L'objet '${nomObjet}' n'existe pas.` })
         }
 
-        // Déconnecte l'objet de la table relationnelle
-        const inventaireMisAJour = await prisma.inventaire.update({
-            where: { personnageId: personnageId },
-            data: {
-                objets: {
-                    disconnect: { id: objet.id }
-                }
-            },
-            include: { objets: true }
+        // Trouver la ligne dans l'inventaire du personnage qui lient cet objet
+        const itemInventaire = await prisma.inventaire.findFirst({
+            where: {
+                idPersonnage: personnageId,
+                idObjet: objet.id
+            }
+        })
+
+        if (!itemInventaire) {
+            return res.status(404).json({ erreur: `Le personnage ne possède pas cet objet dans son inventaire.` })
+        }
+
+        // Supprimer cette l'objet de l'inventaire
+        await prisma.inventaire.delete({
+            where: { id: itemInventaire.id }
         })
 
         return res.status(200).json({ 
-            message: `${objet.nom} a été retiré de l'inventaire.`,
-            inventaire: inventaireMisAJour 
+            message: `L'objet '${objet.nom}' a été retiré de l'inventaire.` 
         })
     } catch (e) {
-        return res.status(500).json({ erreur: `Erreur lors du retrait de l'objet : ${e}. Vérifiez que l'ID du personnage est correct.` })
+        return res.status(500).json({ erreur: `Erreur lors du retrait de l'objet : ${e}.` })
     }
 })
 
