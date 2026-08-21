d0b8a570af7edbb72191d606e0c5da68103fbfc1
Mathieu Gosselin <math.gosselin@gmail.com>
2026-08-20 21:34:56 -0400

    feat: ajout du footer

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -1,3 +1,16 @@
+import "../css/footer.css";
+
 export default function Footer() {
-    return (<><p>Footer à compléter</p></>)
+  return (
+    <footer className="container-footer" style={{ display: "flex", alignContent: "center", flexDirection: "column"}}>
+
+        <div className="serverStatusBadge" style={{ display: "flex", alignItems: "center", gap: "6px"}}>
+            <p className="text-3xl statusDot" style={{color: "green"}}>⦿</p>
+            <p className="statusText" style={{fontWeight: "bold", color: "green"}}>Serveur en ligne</p>
+        </div>
+        <p style={{fontSize: "0.8em", flexGrow: 1, alignSelf:"center", marginBottom: "12px" }}>&copy; 2026 Royaume MaxRemi. Tous droits réservés.</p>
+
+    
+    </footer>
+  );
 }
