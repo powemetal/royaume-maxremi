2443b4735eb7cb6b584bea71864723c79da7a5f4
powemetal <francisbvrt@gmail.com>
2026-08-21 00:43:24 -0400

    [Route] Objet: Travaux majeurs sur la route objet, validation des donnes poussée lors de la creation et du patch, il est dorenavant impossible d'ajouter un champ qui n'existe pas lors d'un patch

--- backend/src/routes/objet.routes.ts
+++ backend/src/routes/objet.routes.ts
@@ -3,53 +3,168 @@ import prisma from "../utils/prisma.js"
 import { authentifier, exigerRole } from "../middlewares/auth.js"
 
 const routeurObjets = Router ()
+const raretesValides = ["COMMUN", "PEU_COMMUN", "RARE", "LEGENDAIRE"]
+const tTypeValides = ["ARME","ARME_2_MAINS","MUNITION","BOUCLIER","ARMURE","BOTTES","CASQUE","GANT","CAPE","BIJOU","TRINKET","POTION","PARCHEMIN","BAGUETTE","BATON","FOCALISATEUR","OUTIL","SAC","CONTENEUR","MARCHANDISE","AUTRE"];
+const typeDegatsValides = ["FEU","GLACE","ACIDE","POISON","TONNERRE","PSYCHIQUE","NECROTIQUE","FORCE","PERCANT","CONTONDANT","TRANCHANT","RADIANT"];
+import { Rarete, TypeDegats, TypeObjet } from "../../generated/prisma/enums.js";
+
+
+
+interface Objet {
+    nom: string, 
+    rarete: Rarete,
+    tType: TypeObjet,
+    att?: number | null,
+    def?: number | null,
+    prix: number,
+    typeDegats?: TypeDegats | null,
+    degatsBonus?: number | null,
+    typeBonus?: TypeDegats | null,
+    description?: string | null,
+}
+
+interface PatchObjet {
+    nom?: string, 
+    rarete?: Rarete,
+    tType?: TypeObjet,
+    att?: number | null,
+    def?: number | null,
+    prix?: number,
+    typeDegats?: TypeDegats | null,
+    degatsBonus?: number | null,
+    typeBonus?: TypeDegats | null,
+    description?: string | null,
+}
+
 
 routeurObjets.post("/objet/creer", authentifier, exigerRole("MAITRE_DU_JEU"), async (req: Request, res: Response) => {
-    const raretesValides = ["COMMUN", "PEU_COMMUN", "RARE", "LEGENDAIRE"]
-    const { nom, rarete } = req.body
+    const {nom, rarete, tType, prix} = req.body;
 
-    if (!nom) {
-        return res.status(400).json({ erreur: "Erreur: Le nom de l'objet est manquant" });
+    if (!nom) return res.status(400).json({ erreur: "Erreur: Le nom de l'objet est manquant" });
+    if (!raretesValides.includes(rarete)) return res.status(400).json({erreur: `Erreur: La rareté de l'objet est invalide`})
+    if (!tTypeValides.includes(tType)) return res.status(400).json({erreur: `Erreur: Le type de l'objet est invalide`})
+    if (typeof prix != "number") return res.status(400).json({erreur: `Erreur: Le prix doit être un nombre`})
+
+
+    const data : Objet = { nom, rarete, tType, prix}
+
+    if (req.body.att !== undefined && ["ARME", "ARME_2_MAINS"].includes(req.body.tType)){
+            if (typeof req.body.att !== "number") return res.status(400).json({ erreur: "L'attaque est invalide"})
+            data.att = req.body.att
+        }
+    if (req.body.def !== undefined && ["ARMURE", "BOTTES", "CASQUE", "BOUCLIER", "GANT"].includes(req.body.tType)){
+            if (typeof req.body.def !== "number") return res.status(400).json({ erreur: "La defense est invalide"})
+            data.def = req.body.def
+        }
+    if (req.body.typeDegats !== undefined) {
+        if (!typeDegatsValides.includes(req.body.typeDegats)) {
+                return res.status(400).json({ erreur: "Type de dégats invalide" });
+            }
+            data.typeDegats = req.body.typeDegats;
+        }
+    if (req.body.degatsBonus !== undefined) {
+        if (typeof req.body.degatsBonus !== "number") {
+            return res.status(400).json({ erreur: "dégats bonus invalide" });
+        } data.degatsBonus = req.body.degatsBonus
     }
+    if (req.body.typeBonus !== undefined) {
+        if (!typeDegatsValides.includes(req.body.typeBonus)) {
+                return res.status(400).json({ erreur: "Dégats bonus invalide" });
+            }
+            data.typeBonus = req.body.typeBonus;
+        }
+    if (req.body.description !== undefined) data.description = req.body.description
     
-    if (!raretesValides.includes(rarete)) {
-        return res.status(400).json({erreur: `Erreur: La rareté de l'objet est invalide`})
-    }
 
-    try {
-        const objet = await prisma.objet.create({ data: { nom, rarete } })
-        return res.status(201).json({ message: `Objet ${nom} créé avec succès !` })
+        try {
+        const objet = await prisma.objet.create({ 
+            data: data 
+            })
+
+        return res.status(201).json({ message: `Objet ${data.nom} créé avec succès !` })
     } catch (error) {
-        res.status(400).json({ message: "Erreur: L'objet n'a pas pu être créé." })
+        res.status(400).json({ erreur: "Erreur: L'objet n'a pas pu être créé." })
     }
 })
 
 
 //Modifier un objet
 routeurObjets.patch("/objet/:nom", authentifier, exigerRole("MAITRE_DU_JEU"), async(req: Request, res: Response)=>{
-    const nom = req.params.nom as string
+    const nomObjet = req.params.nom as string
+    const body = req.body
+
         //Trouver l'objet
     try{
         const objet = await prisma.objet.findFirst({
             where : {
                 nom: {
-                    equals: nom,
+                    equals: nomObjet,
                     mode: "insensitive",    
                 }
             }, 
         })
-        if (!objet){
-            return res.status(404).json({erreur: `Erreur: L'objet ${nom} n'existe pas dans le jeu.`})
+
+        if (!objet) return res.status(404).json({erreur: `Erreur: L'objet ${nomObjet} n'existe pas dans le jeu.`})
+        
+        
+        const data : PatchObjet = {};
+
+        //validation des donnees
+        if (body.nom !== undefined) data.nom = body.nom;
+        if (body.rarete !== undefined) {
+        if (!raretesValides.includes(body.rarete)) {
+            return res.status(400).json({ erreur: "Rareté invalide" });
+            }
+            data.rarete = body.rarete;
+        }
+        if (body.tType !== undefined ){
+            if (!tTypeValides.includes(body.tType)){
+                return res.status(400).json({ erreur: "Type d'objet invalide" });
+            } data.tType = body.tType;
+        } 
+
+        //*Merci Gemini*
+        //Sert a prendre soit le nouveau type ou le type que lobjet avait deja sinon il sera possible de mettre une attaque a un objet de defence et vice versa
+        const typeObjet = body.tType || objet.tType
+        if (body.att !== undefined && ["ARME", "ARME_2_MAINS"].includes(typeObjet)){
+            if (typeof body.att !== "number") return res.status(400).json({ erreur: "L'attaque est invalide"})
+            data.att = body.att
         }
 
+        if (body.def !== undefined && ["ARMURE", "BOTTES", "CASQUE", "BOUCLIER", "GANT"].includes(typeObjet)){
+            if (typeof body.def !== "number") return res.status(400).json({ erreur: "La defense est invalide"})
+            data.def = body.def
+        }
+        if (body.prix !== undefined){
+            if (typeof body.prix !== "number") return res.status(400).json({ erreur: "Le prix doit être un nombre"})
+            data.prix = body.prix
+        }
+
+        if (body.typeDegats !== undefined) {
+            if (!typeDegatsValides.includes(body.typeDegats)) {
+                return res.status(400).json({ erreur: "Type de dégats invalide" });
+            }
+            data.typeDegats = body.typeDegats;
+        }
+
+        if (body.degatsBonus !== undefined ){
+            if (typeof body.degatsBonus !== "number") return res.status(400).json({ erreur: "Type de dégats bonus invalide"})
+            data.degatsBonus = body.degatsBonus
+        }
+        if (body.typeBonus !== undefined){
+            if (!typeDegatsValides.includes(body.typeBonus)) {
+                return res.status(400).json({ erreur: "Type de dégats invalide" });
+            } 
+            data.typeBonus = body.typeBonus;
+        } 
+        if (body.description !== undefined) data.description = body.description;
         //Modifie l'objet
         const objetModifie = await prisma.objet.update({
-            where: { id:objet.id},
-            data: req.body
+            where: { id: objet.id},
+            data: data
+                
         })
-
         res.status(200).json(objetModifie)
-
 
     } catch(e){
         res.status(500).json({erreur: `Erreur: Le serveur ne répond pas lors de la modification de l'objet : ${e}`})
