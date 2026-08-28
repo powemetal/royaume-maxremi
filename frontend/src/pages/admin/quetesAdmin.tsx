b0d936f65b594d92aab3e0eaec412ea7745da8ac
Mathieu Gosselin <math.gosselin@gmail.com>
2026-08-28 15:24:58 -0400

    feature: admin/quetes visuel complété

--- frontend/src/pages/admin/quetesAdmin.tsx
+++ frontend/src/pages/admin/quetesAdmin.tsx
@@ -1,3 +1,5 @@
+import { Select } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
 import TitreBackground from "../../components/titreBackground"
 import { useAuth } from "../../context/AuthContext";
 import { useState, useEffect, useMemo } from "react"
@@ -181,7 +183,9 @@ export default function QuetesAdmin() {
     useEffect(() => {
         if (message) {
             setMessageVisible(true);
-            const timer = setTimeout(() => setMessageVisible(false), 4000);
+            const timer = setTimeout(() => {
+                setMessageVisible(false),
+                setMessage("");}, 4000);
             return () => clearTimeout(timer);
         }
     }, [message]);
@@ -189,7 +193,9 @@ export default function QuetesAdmin() {
     useEffect(() => {
         if (messageModifier) {
             setMessageModifierVisible(true);
-            const timer = setTimeout(() => setMessageModifierVisible(false), 4000);
+            const timer = setTimeout(() => {
+                setMessageModifierVisible(false);
+                setMessageModifier("");}, 4000);
             return () => clearTimeout(timer);
         }
     }, [messageModifier]);
@@ -197,7 +203,10 @@ export default function QuetesAdmin() {
     useEffect(() => {
         if (erreur) {
             setErreurVisible(true);
-            const timer = setTimeout(() => setErreurVisible(false), 4000);
+            const timer = setTimeout(() => {
+                setErreurVisible(false);
+                setErreur("");
+            }, 4000);
             return () => clearTimeout(timer);
         }
     }, [erreur]);
@@ -209,26 +218,45 @@ export default function QuetesAdmin() {
     return (<>
     <div className="container-admin-quetes flex flex-col grow">
         <TitreBackground>Ajouter une quête</TitreBackground>
-        <div className="container-ajout-quete flex flex-col container-style">
+        <div className="container-ajout-quete flex flex-col w-full container-style text-white">
 
             <form onSubmit={handleSubmitAjout} className="form-container">
-                <div className="container-ajout-champs flex space-x-8 justify-evenly shrink mb-8 mt-8">
-                    <label htmlFor="nom">Nom:</label>
-                    <input type="text" id="nom-ajout" name="nom" value={formDataAjout.nom} onChange={handleChangeAjout} required placeholder="Ex: Dératiser le champ"/>
-                    <label htmlFor="difficulte">Difficulté:</label>
-                    <select onChange={handleChangeAjout} value={formDataAjout.difficulte} name="difficulte" id="difficulte-ajout" required>
-                        {Object.entries(DIFFICULTE).map(([key, value]) => (
-                            <option key={key} value={value}>
-                                {key.charAt(0) + key.slice(1).toLowerCase()}
-                            </option>
-                        ))}
-                    </select>
-                    <label htmlFor="recompense">Récompense:</label>
-                    <input type="text" name="recompense" id="recompense-ajout" value={formDataAjout.recompense} onChange={handleChangeAjout} required placeholder="500" />
+                <div className="container-ajout-champs flex justify-evenly space-x-8 shrink m-8">
+
+                    <div className="flex flex-col gap-1 flex-2">
+                        <label htmlFor="nom" className="form-labels">Nom:</label>
+                        <input type="text" id="nom-ajout" name="nom" value={formDataAjout.nom} onChange={handleChangeAjout} className="font-sans block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/20 placeholder:text-white/60 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm" required placeholder="Ex: Dératiser le champ"/>
+                    </div>
+                    
+
+                    <div className="flex flex-col gap-1">
+                        <label htmlFor="difficulte-ajout" className="form-labels">
+                            Difficulté:
+                        </label>
+
+                        <div className="relative">
+                            <Select id="difficulte-ajout" name="difficulte" value={formDataAjout.difficulte} onChange={handleChangeAjout} className="inline-flex w-full appearance-none items-center justify-between rounded-md bg-white/10 px-3 py-2 text-sm font-semibold text-white shadow-xs outline-1 -outline-offset-1 outline-white/20 focus:outline-2 focus:outline-indigo-500">
+                                {Object.entries(DIFFICULTE).map(([key, value]) => (
+                                    <option key={key} value={value} className="bg-gray-800 text-white">
+                                        {key.charAt(0) + key.slice(1).toLowerCase()}
+                                    </option>
+                                ))}
+                            </Select>
+                            <ChevronDownIcon className="group-pointer-events-none absolute top-2.5 right-2.5 size-5 fill-white/60" aria-hidden="true"/>
+                        </div>
+                    </div>
+
+
+                        <div className="flex flex-col gap-1">
+                            <label htmlFor="recompense" className="form-labels">Récompense:</label>
+                            <input type="text" name="recompense" id="recompense-ajout" value={formDataAjout.recompense} onChange={handleChangeAjout} className="font-sans block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/20 placeholder:text-white/60 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm" required placeholder="500" />
+                        </div>
                 </div>
+
+
                 <div className="container-ajout-description flex flex-col mb-8">
-                    <label htmlFor="description" className="ml-8">Description:</label>
-                    <textarea name="description" id="description-ajout" rows={10} cols={50} value={formDataAjout.description} onChange={handleChangeAjout} placeholder="Le fermier vous demande..."></textarea>
+                    <label htmlFor="description" className="ml-8 mt-4 form-labels">Description:</label>
+                    <textarea className="p-6 min-h-80 resize-none" name="description" id="description-ajout" value={formDataAjout.description} onChange={handleChangeAjout} placeholder="Le fermier vous demande..."/>
                 </div>
                 <div className="container-ajout-bouton flex justify-end mr-8 mb-8">
                     <button className="btn-nav create disabled:opacity-50 disabled:cursor-not-allowed disabled:pointer-events-none disabled:grayscale" disabled={!formulaireAjoutEstValide()} >Ajouter</button>
@@ -243,46 +271,56 @@ export default function QuetesAdmin() {
 
 
         <TitreBackground>Modifier une quête</TitreBackground>
-        <div className="container-modif-quete container-style flex flex-col">
+        <div className="container-modif-quete container-style flex flex-col text-white">
 
             <form className="modif-form-container" onSubmit={handleSubmitModifier}>
-                <div className="container-modif-champs-liste flex">
-                    <div className="container-modif-gauche flex flex-2 flex-col">
-                        <div className="modif-nom flex">
-                            <label htmlFor="nom">Nom:</label>
-                            <input type="text" id="nom-modifier" name="nom" value={formDataModifier.nom} onChange={handleChangeModifier} required placeholder="Ex: Dératiser le champ"/>
+                <div className="container-modif-champs-liste flex max-h-200">
+                    <div className="container-modif-gauche min-w-0 flex flex-2 flex-col">
+                        <div className="modif-nom flex flex-col mx-8">
+                            <label htmlFor="nom" className="text-2xl form-labels ml-8 mt-8 mb-4">Nom:</label>
+                            <input type="text" id="nom-modifier" name="nom" value={formDataModifier.nom} onChange={handleChangeModifier} className="font-sans block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/20 placeholder:text-white/60 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm" required placeholder="Ex: Dératiser le champ"/>
                         </div>
-                        <div className="modif-diff flex">
-                            <label htmlFor="difficulte">Difficulte:</label>
-                            <select onChange={handleChangeModifier} value={formDataModifier.difficulte} name="difficulte" id="difficulte-modifier" required>
-                                {Object.entries(DIFFICULTE).map(([key, value]) => (
-                                    <option key={key} value={value}>
-                                        {key.charAt(0) + key.slice(1).toLowerCase()}
-                                    </option>
-                                ))}
-                            </select>
-                        </div>                        
-                        <div className="modif-nom flex">
-                            <label htmlFor="recompense">Récompense:</label>
-                            <input type="text" id="recompense-modifier" name="recompense" value={formDataModifier.recompense} onChange={handleChangeModifier} required placeholder="500"/>
+
+                            <div className="flex gap-1 flex-col max-w-50">
+                                <label htmlFor="difficulte-ajout" className="ml-8 mt-4 mb-4 form-labels">
+                                    Difficulté:
+                                </label>
+
+                                <div className="relative">
+                                    <Select id="difficulte-modifier" name="difficulte" value={formDataModifier.difficulte} onChange={handleChangeModifier} className="ml-8 inline-flex w-full appearance-none items-center justify-between rounded-md bg-white/10 px-3 py-2 text-sm font-semibold text-white shadow-xs outline-1 -outline-offset-1 outline-white/20 focus:outline-2 focus:outline-indigo-500">
+                                    
+                                        {Object.entries(DIFFICULTE).map(([key, value]) => (
+                                            <option key={key} value={value} className="block w-full text-left px-4 py-2 text-sm text-white data-focus:bg-white/10 data-focus:text-white data-focus:outline-hidden">
+                                                {key.charAt(0) + key.slice(1).toLowerCase()}
+                                            </option>
+                                        ))}
+                                    </Select>
+                                    <ChevronDownIcon className="group-pointer-events-none absolute top-3 -right-6 size-5 fill-white/60" aria-hidden="true"/>
+                                </div>
+                            </div>
+
+                       
+                        <div className="modif-nom flex flex-col max-w-50">
+                            <label htmlFor="recompense" className="form-labels ml-8 mt-4 mb-4">Récompense:</label>
+                            <input type="text" id="recompense-modifier" name="recompense" value={formDataModifier.recompense} onChange={handleChangeModifier} className=" ml-8 mr-8 flex-end font-sans block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/20 placeholder:text-white/60 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm"required placeholder="500"/>
                         </div>
-                        <div className="modif-description flex flex-col">
-                            <label htmlFor="description">Description:</label>
-                            <textarea name="description" id="description-modifier" rows={10} cols={25} value={formDataModifier.description} onChange={handleChangeModifier} placeholder="Le fermier vous demande..."></textarea>
+                        <div className="modif-description flex flex-col flex-1 mb-8">
+                            <label htmlFor="description" className="form-labels ml-8 mt-4">Description:</label>
+                            <textarea className="flex-1 p-6 resize-none" name="description" id="description-modifier" value={formDataModifier.description} onChange={handleChangeModifier} placeholder="Le fermier vous demande..."/>
                         </div>
                     </div>
 
 
 
-                    <div className="container-modif-droite flex flex-1 inner-container m-8">
-                        <ul className="w-full">
+                    <div className="container-modif-droite flex flex-1 inner-container m-8 min-w-0">
+                        <ul className="w-full liste-quetes flex flex-col">
                             {erreur && (<span className={`msg flex justify-center transition-opacity duration-300 ${erreurVisible ? "opacity-100" : "opacity-0"}`}>{erreur}</span>)}
                             {!chargement && listeQuetes.length === 0 && (
                                 <span className="perso-nom">Aucune quête à afficher.</span>
                             )}
                             {listeQuetesTriee.map((quete) => {
                                 return (
-                            <li key={quete.id} className="quetes-modif-list" onClick={() => handleSelectionnerQuete(quete)}>{quete.nom} [ {quete.difficulte} ]`</li>
+                            <li key={quete.id} className="quetes-modif-list" onClick={() => handleSelectionnerQuete(quete)}>{quete.nom}<br /><span className="diff">[  {quete.difficulte}  ]</span></li>
                             )})}
                         </ul>
                     </div>
