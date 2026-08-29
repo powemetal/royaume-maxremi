0da082d65bb6b2a3eb4cfa8629865731f2996be2
Clement Laflamme <clement_laflamme@videotron.ca>
2026-08-29 18:08:17 -0400

    feat: logique objets

--- frontend/src/components/barreRecherche.tsx
+++ frontend/src/components/barreRecherche.tsx
@@ -1,30 +1,49 @@
+import { useState } from "react";
+
 interface BarreRechercheProps {
   placeholder?: string;
   className?: string;
+  onSearch: (valeur: string) => void;
 }
 
 export default function BarreRecherche({
   placeholder = "Rechercher...",
   className,
+  onSearch,
 }: BarreRechercheProps) {
+  const [search, setSearch] = useState("");
   return (
-    <div
-      className={`flex items-center border pl-3 gap-2 bg-white border-gray-500/30 h-[46px] rounded-md overflow-hidden w-full ${className}`}
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="22"
-        height="22"
-        viewBox="0 0 30 30"
-        fill="#6B7280"
+    <div className="flex gap-2 items-center">
+      <div
+        className={`flex items-center border pl-3 gap-2 bg-white border-gray-500/30 h-[46px] rounded-md overflow-hidden w-full ${className}`}
       >
-        <path d="M13 3C7.489 3 3 7.489 3 13s4.489 10 10 10a9.95 9.95 0 0 0 6.322-2.264l5.971 5.971a1 1 0 1 0 1.414-1.414l-5.97-5.97A9.95 9.95 0 0 0 23 13c0-5.511-4.489-10-10-10m0 2c4.43 0 8 3.57 8 8s-3.57 8-8 8-8-3.57-8-8 3.57-8 8-8" />
-      </svg>
-      <input
-        type="text"
-        placeholder={placeholder}
-        className="w-full h-full outline-none text-gray-500 placeholder-gray-500 text-sm"
-      />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="22"
+          height="22"
+          viewBox="0 0 30 30"
+          fill="#6B7280"
+        >
+          <path d="M13 3C7.489 3 3 7.489 3 13s4.489 10 10 10a9.95 9.95 0 0 0 6.322-2.264l5.971 5.971a1 1 0 1 0 1.414-1.414l-5.97-5.97A9.95 9.95 0 0 0 23 13c0-5.511-4.489-10-10-10m0 2c4.43 0 8 3.57 8 8s-3.57 8-8 8-8-3.57-8-8 3.57-8 8-8" />
+        </svg>
+        <input
+          type="text"
+          placeholder={placeholder}
+          className="w-full h-full outline-none text-gray-500 placeholder-gray-500 text-sm cinzel"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+      <button
+        type="submit"
+        className="btn-nav degrade-rouge w-32 h-[48px] text-sm text-white"
+        onClick={() => {
+          onSearch(search);
+          setSearch("");
+        }}
+      >
+        Rechercher
+      </button>
     </div>
   );
 }
