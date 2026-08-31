8b800e32824c35c539d98ffc7dcb3f144d104243
powemetal <francisbvrt@gmail.com>
2026-08-31 18:05:30 -0400

    [Footer] Ajout de l'indicateur si l<api est en ligne ou non dans le footer

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -1,16 +1,37 @@
+import { useEffect, useState } from "react";
 import "../css/footer.css";
+import { api } from "../api/backendApi";
 
 export default function Footer() {
+  const [estOnline, setEstOnline] = useState<boolean>(false);
+
+  const apiOnline = async () => {
+    try {
+      const check = await api.get("/api/status/");
+      if (check.data.dndApi.status === "up") {
+        setEstOnline(true);
+      } else {
+        setEstOnline(false);
+      }
+    } catch (e) {
+      console.error(`Erreur avec l'api: ${e}`);
+      setEstOnline(false);
+    }
+  };
+
+  useEffect(() => {
+    apiOnline();
+  }, []);
+
   return (
-    <footer className="container-footer" style={{ display: "flex", alignContent: "center", flexDirection: "column"}}>
+    <footer className="container-footer flex flex-col items-center py-4">
+      <div className="serverStatusBadge flex items-center gap-2">
+        <p className={`text-3xl statusDot ${estOnline ? "text-green-500" : "text-red-500"}`}>⦿</p>
 
-        <div className="serverStatusBadge" style={{ display: "flex", alignItems: "center", gap: "6px"}}>
-            <p className="text-3xl statusDot" style={{color: "green"}}>⦿</p>
-            <p className="statusText" style={{fontWeight: "bold", color: "green"}}>Serveur en ligne</p>
-        </div>
-        <p style={{fontSize: "0.8em", flexGrow: 1, alignSelf:"center", marginBottom: "12px" }}>&copy; 2026 Royaume MaxRemi. Tous droits réservés.</p>
+        <p className={`statusText font-bold ${estOnline ? "text-green-500" : "text-red-500"}`}>{estOnline ? "Serveur en ligne" : "Serveur hors ligne"}</p>
+      </div>
 
-    
+      <p className="text-xs mt-2">&copy; 2026 Royaume MaxRemi. Tous droits réservés.</p>
     </footer>
   );
 }
